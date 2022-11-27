0d904846c7a7be4016b22d46dcfbad07878e6074
JazlynL <lubenjazlyn35@gmail.com>
2022-11-27 11:45:21 -0800

    v1 yahtzee build completed cup class.

---
+++ src/Cup.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Cup {
+
+    List<Die> dice = new ArrayList<>();
+
+
+    public Cup(){
+        while(dice.size() < 5){
+            dice.add(new Die());
+        }
+    }
+
+    public void roll(){
+        for(Die die : dice){
+            die.roll();
+        }
+    }
+
+
+
+
+    // overloading example... same method name but different parameters
+    public void roll( int selection){
+        dice.get(selection).roll();
+    }
+
+
+    public void roll(List<Integer> selections){
+        for(int selection : selections){
+            roll(selection);
+        }
+    }
+
+    public String displayCup(){
+        String output = "";
+        for(Die die: dice){
+            output += die.faceUpValue + " ";
+        }
+        return output.trim();
+    }
+
+    public List<Integer> parseInteger(String input){
+        String [] inputarr =   input.split(" ");
+
+
+        List<Integer> arrList = new ArrayList<>();
+        for(String number: inputarr){
+            arrList.add(Integer.parseInt(number)-1);
+        }
+  return  arrList;
+    }
+}
